aba6a8658f9e764effa135e69d53786c34576252
Tom <thomas.spencer@codurance.com>
2020-08-28 23:04:18 +0100

    Add boundary conditions

--- test/leapyear_test.clj
+++ test/leapyear_test.clj
@@ -15,9 +15,15 @@
 
 (deftest leapyear-tests
   (testing "A year is not a leap year if not divisible by 4"
-    (is (= (leap-year? 1995) false)))
+    (is (= (leap-year? 1995) false))
+    (is (= (leap-year? 1) false)))
+
   (testing "A year is a leap year if divisible by 4"
-    (is (= (leap-year? 1996) true)))
+    (is (= (leap-year? 1996) true))
+    (is (= (leap-year? 0) true))
+    (is (= (leap-year? 4) true))
+    (is (= (leap-year? 2020) true)))
+
   (testing "A year is a leap year if divisible by 400",
     (is (= (leap-year? 1600) true)))
 
